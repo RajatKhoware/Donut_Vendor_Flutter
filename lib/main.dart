83e6e6353379f34fc199a3690fa37e22d6bc0151
Rajat Khoware <rajatkhoware2002@gmail.com>
2023-01-29 20:56:03 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Donut Applicationss',
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
